bcb952ebdabfaa87460f6927888b12313d6111bd
George <georgmel4757@gmail.com>
2022-05-30 20:39:53 +0300

    update numToEng function

--- Chapter 2/ex-7.py
+++ Chapter 2/ex-7.py
@@ -27,7 +27,8 @@ def numToEng(number):
         "4": "forty", "5": "fifty", "6": "sixty",
         "7": "seventy", "8": "eighty", "9": "ninety"}
     
-    exclusion = {"10": "ten", "11": "eleven", "12": "twelve",
+    exclusion = {
+        "10": "ten", "11": "eleven", "12": "twelve",
         "13": "thirteen", "15": "fifteen"}
         
     result = ""
@@ -36,41 +37,40 @@ def numToEng(number):
         return None
 
     num_str = str(number)
+    length_num = len(num_str)
 
-    if len(num_str) == 3:
-        result += "{0} hundred ".format(zero_nine[num_str[0]])
-        num_str = num_str[1:]
-
-    if len(num_str) == 2:
-        first_sign = num_str[0]
-        second_sign = num_str[1]
-        if num_str in exclusion.keys():
-            result += "{0}".format(exclusion[num_str])
-            num_str = ""
-        
-        elif first_sign == "0":
-            num_str = second_sign
-        
-        elif second_sign == "0":
-            result += "{0}".format(twenty_ninety[first_sign])
-            num_str = ""
-
-        elif first_sign == '1' and second_sign in "46789":
-            if zero_nine[second_sign][-1] != "t":
-                result += "{0}teen".format(zero_nine[second_sign])
+    while len(num_str) != 0:
+        if len(num_str) == 3:
+            result += "{0} hundred ".format(zero_nine[num_str[0]])
+            num_str = num_str[1:]
+            
+        elif len(num_str) == 2:
+            first_sign = num_str[0]
+            second_sign = num_str[1]
+            
+            if num_str in exclusion:
+                result += "{0}".format(exclusion[num_str])
+                num_str = ""
+            elif first_sign == "0":
+                num_str = second_sign
+            elif first_sign == "1" and second_sign in "46789":
+                if zero_nine[second_sign][-1] != "t":
+                    result += "{0}teen".format(zero_nine[second_sign])
+                else:
+                    result += "{0}een".format(zero_nine[second_sign])
+                num_str = ""
             else:
-                result += "{0}een".format(zero_nine[second_sign])
+                result += "{0} ".format(twenty_ninety[first_sign])
+                num_str = second_sign
+
+        elif len(num_str) == 1:
+            if num_str != "0":
+                result += "{0}".format(zero_nine[num_str])
+            elif length_num == 1 and num_str == "0":
+                result = "zero"
             num_str = ""
 
-        else:
-            result += "{0} {1}".format(
-                twenty_ninety[first_sign], zero_nine[second_sign])
-            num_str = ""
-    
-    if len(num_str) == 1:
-        result += "{0}".format(zero_nine[num_str])
-
-    return result
+    return result.strip()
 
 
 if __name__ == "__main__":
